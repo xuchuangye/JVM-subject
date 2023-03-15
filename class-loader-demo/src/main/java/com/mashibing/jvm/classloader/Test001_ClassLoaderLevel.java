3fe4fefac0f3693ecd7774c7e2b7ff3323203c0d
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2023-03-15 19:51:53 +0800

    JVM调优：类加载器的级别，父加载器既不是类加载器的加载器，也不是类加载器的父类加载器

--- class-loader-demo/src/main/java/com/mashibing/jvm/classloader/Test001_ClassLoaderLevel.java
+++ class-loader-demo/src/main/java/com/mashibing/jvm/classloader/Test001_ClassLoaderLevel.java
@@ -5,7 +5,7 @@ package com.mashibing.jvm.classloader;
  * @author xcy
  * @date 2023/3/15 - 16:43
  */
-public class ClassLoaderLevel {
+public class Test001_ClassLoaderLevel {
 	public static void main(String[] args) {
 		//BootStrap类加载器是C++实现的，Java中并没有对应的class，所以是null
 		//null，表示最顶层的类加载器BootStrap
@@ -14,11 +14,11 @@ public class ClassLoaderLevel {
 		//sun.net.spi.nameservice.dns.DNSNameService.class 位于 ext/目录下的jar包，所以类加载器是ExtClassLoader
 		System.out.println(sun.net.spi.nameservice.dns.DNSNameService.class.getClassLoader());
 		//自定义类默认都是classpath目录下的，所以类加载器是AppClassLoader
-		System.out.println(ClassLoaderLevel.class.getClassLoader());
+		System.out.println(Test001_ClassLoaderLevel.class.getClassLoader());
 
 		//类加载器的加载器
 		System.out.println(sun.net.spi.nameservice.dns.DNSNameService.class.getClassLoader().getClass().getClassLoader());
-		System.out.println(ClassLoaderLevel.class.getClassLoader().getClass().getClassLoader());
+		System.out.println(Test001_ClassLoaderLevel.class.getClassLoader().getClass().getClassLoader());
 
 	}
 }
